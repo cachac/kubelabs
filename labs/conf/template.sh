3a4633c5968c99878a46c556f25d0aceccac2030
car <carlos.echc11@gmail.com>
2021-11-23 20:12:52 -0600

    sh brew

--- labs/conf/template.sh
+++ labs/conf/template.sh
@@ -31,22 +31,24 @@ sudo chown -f -R ${username} ~/.kube
 runuser -l ${username} -c "newgrp microk8s"
 
 # config remote access to kubectl
-echo "external IP ${external_ip}" >> /home/${username}/ilog
+echo "config context external IP ${external_ip}" >> /home/${username}/ilog
 sudo microk8s kubectl config set-cluster microk8s-cluster --server=https://${external_ip}:16443 --insecure-skip-tls-verify
 # no es necesario editar los certificados. Solo se tiene que agregar el servidor con la ip externa en el cluster (set-cluster)
 # sudo sed -e "s/#MOREIPS/IP.3 = ${external_ip}/" /var/snap/microk8s/current/certs/csr.conf.template
 
 echo "Microk8s + Kubectl!!" >> /home/${username}/ilog
+date '+%Y/%m/%d %H:%M:%S %z' >> /home/${username}/ilog
 
 # kubecolor
 echo "Instaling Brew + Kubecolor..." >> /home/${username}/ilog
 sudo apt-get install -y build-essential
 runuser -l ${username} -c '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" \ </dev/null'
 echo "Brew!!!" >> /home/${username}/ilog
-date '+%Y/%m/%d %H:%M:%S %z' > /home/${username}/ilog
+date '+%Y/%m/%d %H:%M:%S %z' >> /home/${username}/ilog
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/${username}/.profile
-brew install gcc
-brew install hidetatz/tap/kubecolor
+runuser -l ${username} -c 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
+runuser -l ${username} -c 'brew install gcc'
+runuser -l ${username} -c 'brew install hidetatz/tap/kubecolor'
 echo "Kubecolor!!!" >> /home/${username}/ilog
 
 echo 'function kubecolor() { echo "+ kubectl $@">&2; command kubecolor $@; }' >> /home/${username}/.bashrc
@@ -54,4 +56,4 @@ runuser -l ${username} -c  'complete -o default -F __start_kubectl kubecolor'
 runuser -l ${username} -c  'complete -o default -F __start_kubectl k'
 
 echo "*** FIN ***" >> /home/${username}/ilog
-date '+%Y/%m/%d %H:%M:%S %z' > /home/${username}/ilog
+date '+%Y/%m/%d %H:%M:%S %z' >> /home/${username}/ilog
