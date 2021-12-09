7aa4487411db0f369c2716887a4d9c4ad62b760b
car <carlos.echc11@gmail.com>
2021-12-09 17:03:15 -0600

    valida

--- dev/frontend/src/components/arch.vue
+++ dev/frontend/src/components/arch.vue
@@ -2,23 +2,12 @@
   <div>
     <h1>Arquitectura Cloud</h1>
     <!-- <img alt="Kubelabs logo" src="../assets/kubelabs.png" /> -->
-    <v-zoomer style="width: 1000px; height: 500px;">
+    <v-zoomer style="width: 1000px; height: 500px">
       <img src="../assets/kubelabs.png" style="object-fit: contain; width: 100%; height: 100%" />
     </v-zoomer>
   </div>
 </template>
 
 <script>
-import reactUser from "./user";
-
-export default {
-  components: {},
-  setup() {
-    const { list, loadData } = reactUser();
-    return { list, loadData };
-  },
-  mounted() {
-    this.loadData();
-  },
-};
+export default {};
 </script>
