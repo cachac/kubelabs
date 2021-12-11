2e67c399ae5095992932cfc652155d850c6572d5
car <carlos.echc11@gmail.com>
2021-12-10 22:30:44 -0600

    cc

--- labs/x/data.tf
+++ labs/x/data.tf
@@ -3,14 +3,3 @@ data "google_compute_image" "master_image" {
   family = "ubuntu-2004-lts"
   project = "ubuntu-os-cloud"
 }
-
-resource "google_compute_disk" "kube-disk" {
-  name  = "kube-disk-default"
-  type  = "pd-standard"
-  zone  = var.gcp_zone_a
-	size = "30"
-  labels = {
-    environment = "dev"
-  }
-  physical_block_size_bytes = 4096
-}
