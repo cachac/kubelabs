0ea60f905b1625fa048a9f93f64d8a0f28fbf19f
car <carlos.echc11@gmail.com>
2022-03-23 22:34:05 -0600

    z

--- assets/x1/run.sh
+++ assets/x1/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
-kubectl apply -f assets/18/api-ingress.yml
-kubectl apply -f assets/18/api-svc.yml
-kubectl apply -f assets/18/api-deploy.yml
+kubectl apply -f assets/x1/api-ingress.yml
+kubectl apply -f assets/x1/api-svc.yml
+kubectl apply -f assets/x1/api-deploy.yml
 
-kubectl apply -f assets/18/web-ingress.yml
-kubectl apply -f assets/18/web-svc.yml
-kubectl apply -f assets/18/web-deploy.yml
+kubectl apply -f assets/x1/web-ingress.yml
+kubectl apply -f assets/x1/web-svc.yml
+kubectl apply -f assets/x1/web-deploy.yml
 
 watch kubectl get pods -l app=fulldemo
